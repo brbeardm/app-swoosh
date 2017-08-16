87f7f860219fe4782117b3947610f0e8eba69269
Brian Beardmore <bbeardmore@carvizor.com>
2017-08-15 22:59:04 -0500

    Lesson-06-07-08

--- app-swoosh/app-swoosh/Controller/WelcomeVC.swift
+++ app-swoosh/app-swoosh/Controller/WelcomeVC.swift
@@ -8,9 +8,9 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-    @IBOutlet weak var swoosh: UIImageView!
-    @IBOutlet weak var bgImg: UIImageView!
+class WelcomeVC: UIViewController {
+//    @IBOutlet weak var swoosh: UIImageView!
+//    @IBOutlet weak var bgImg: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
