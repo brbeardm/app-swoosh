c5555d158e28e29be032345b975b436984a07e83
Brian Beardmore <bbeardmore@carvizor.com>
2017-08-15 23:27:07 -0500

    Lesson-09 structs
    structs and setting up buttons to set the struct value

---
+++ app-swoosh/app-swoosh/Model/Player.swift
@@ -0,0 +1,14 @@
+//
+//  Player.swift
+//  app-swoosh
+//
+//  Created by Brian Beardmore on 8/15/17.
+//  Copyright © 2017 Brian Beardmore. All rights reserved.
+//
+
+import Foundation
+
+struct Player {
+    var desiredLeague: String?
+    var selectedSkillLevel: String?
+}
